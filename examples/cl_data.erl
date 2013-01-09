27052eac22dec425f16da21f90cfe3948d9cbff8
Edward Tate <edward.tate@erlang-solutions.com>
2013-01-09 08:54:36 +0000

    Made cl_data support basic integer types.

--- examples/cl_data.erl
+++ examples/cl_data.erl
@@ -1,6 +1,6 @@
 -module(cl_data).
 
--export([create_buffer/4, buffer_mem/1]).
+-export([create_buffer/4, kernel_args/1]).
 
 %%------------------------------------------------------------------------------
 %% Generic type descriptors
@@ -10,10 +10,16 @@ create_buffer(Context, Data, TypeSize, Opt) ->
   {ok, Mem} = cl:create_buffer(Context, Opt, DataSize),
   {buffer_desc, Mem, Data, DataSize}.
 
-buffer_mem({buffer_desc, Mem, _, _}) ->
-  Mem;
-buffer_mem(BufferDescriptors) when is_list(BufferDescriptors) ->
-  [Mem || {buffer_desc, Mem, _, _} <- BufferDescriptors].  
+
+kernel_args(BufferDescriptors) ->
+  kernel_args(BufferDescriptors, []).
+
+kernel_args([], Acc) ->
+  lists:reverse(Acc);
+kernel_args([{ok, Atom}|Rest], Acc) ->
+  kernel_args(Rest, [Atom|Acc]);
+kernel_args([{buffer_desc, Mem, _, _}|Rest], Acc) ->
+  kernel_args(Rest, [Mem|Acc]).
 
   
 
