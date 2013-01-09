1aeecaec12caf3ccae9874d2f00dd45c18c08617
Edward Tate <edward.tate@erlang-solutions.com>
2013-01-09 07:04:13 +0000

    Refined the structure of a kernel in preparation for parse_trans.

--- examples/cl_mult_kernel.erl
+++ examples/cl_mult_kernel.erl
@@ -3,8 +3,11 @@
 %%------------------------------------------------------------------------------
 -module(cl_mult_kernel).
 
--export([source/0, data_descriptors/0]).
+-export([source/0, input/0, output/0]).
 
+%%------------------------------------------------------------------------------
+%% Sample dimensions
+%%------------------------------------------------------------------------------
 -define(WA, 128).
 -define(HA, 128).
 -define(WB, 128).
@@ -12,23 +15,32 @@
 -define(WC, 128).
 -define(HC, 128).
 
+%%------------------------------------------------------------------------------
+%% Sample data
+%%------------------------------------------------------------------------------
 matA() ->
   << <<X:32/native-float>> || X <- lists:seq(1, ?WA * ?HA) >>.
 matB() ->
   << <<X:32/native-float>> || X <- lists:seq(1, ?WB * ?HB) >>.
 matC() ->
   << <<X:32/native-float>> || X <- lists:seq(1, ?WC * ?HC) >>.
-rowA() ->
-  <<"0">>.
-colC() ->
-  <<"0">>.
+widthA() ->
+  << <<X:32/integer>> || X <- lists:seq(0, 1) >>.
+widthB() ->
+  << <<X:32/integer>> || X <- lists:seq(0, 1) >>.
 
+%%------------------------------------------------------------------------------
+%% Kernel source
+%%------------------------------------------------------------------------------
 source() ->
   "__kernel void mmult(
-     __global float* C, 
+     /* Input */
      __global float* A, 
      __global float* B, 
-     int widthA, int widthB) 
+     int widthA, int widthB,
+     /* Output */
+     __global float* C     
+   ) 
    {
      int i = get_global_id(0);
      int j = get_global_id(1);
@@ -40,9 +52,14 @@ source() ->
      C[i + widthA * j] = value;
    }".
 
-data_descriptors() ->
-  [cl_util:data_desc(matC(), 4, [read_write]),
-   cl_util:data_desc(matA(), 4, [read_write]),
-   cl_util:data_desc(matB(), 4, [read_write]),
-   cl_util:data_desc(rowA(), 4, [read_write]),
-   cl_util:data_desc(colC(), 4, [read_write])].
+%%------------------------------------------------------------------------------
+%% Kernel parameters
+%%------------------------------------------------------------------------------
+input() ->
+  [{matA(), 4, [read_only]},
+   {matB(), 4, [read_only]},
+   {widthA(), 4, [read_only]},
+   {widthB(), 4, [read_only]}].
+
+output() ->
+  [{matC(), 4, [write_only]}].
