0abf195f2a48860f5f0e3be5a14886c6df6044d0
Edward Tate <edward.tate@erlang-solutions.com>
2013-01-09 09:27:22 +0000

    Added resource freeing to the mult example.

--- examples/cl_mult.erl
+++ examples/cl_mult.erl
@@ -8,37 +8,43 @@
 -define(LOCAL_WORK_SIZE, 16).
 
 %%------------------------------------------------------------------------------
+%% Sets up the OpenCL context, creates input and output buffers, ...
+%%------------------------------------------------------------------------------
 test() ->
   Cl = clu:setup(all),
 
-  %%
-  %% Note: each kernel must have a source() and parameters() where parse_trans
-  %%       is concerned.
-  %%
+  %%------------------------------------------------------------------------------
+  %% Note: Each kernel may have a source(), input() and output() parameters or
+  %%       a similar API for a parse transform to be created.
+  %%------------------------------------------------------------------------------
   KernelSource = cl_mult_kernel:source(),
   KernelI = cl_mult_kernel:input(),
   KernelO = cl_mult_kernel:output(),
 
   io:format("Building kernel from source...~n"),
-  Kernel = cl_util:build_kernel("mmult", Cl#cl.context, KernelSource, Cl#cl.devices),
+  {ok, Program, Kernel} = 
+    cl_util:build_kernel("mmult", Cl#cl.context, KernelSource, Cl#cl.devices),
 
   I = cl_util:create_buffers(Cl#cl.context, KernelI),
   O = cl_util:create_buffers(Cl#cl.context, KernelO),
 
-  CommandQueues = 
+  Queues = 
     lists:map(
       fun (Device) ->
-	  {ok, CommandQueue} = cl:create_queue(Cl#cl.context, Device, []),
-	  CommandQueue
+	  {ok, Queue} = cl:create_queue(Cl#cl.context, Device, []),
+	  Queue
       end, Cl#cl.devices
      ),
   
   lists:foreach(
-    fun (CommandQueue) ->
-	{ok, E} = execute_kernel_directives(CommandQueue, Kernel, I, O),
-	cl:wait(E)
-    end, CommandQueues
-   ).
+    fun (Queue) ->
+	execute_kernel_directives(Queue, Kernel, I, O)
+    end, Queues
+   ),
+
+  cl_util:release(Program, Kernel, Queues, I, O),
+
+  clu:teardown(Cl).
 
 execute_kernel_directives(Queue, Kernel, InDescriptors, OutDescriptors) ->  
   io:format("Executing kernel directives...~n"),
@@ -70,9 +76,5 @@ execute_kernel_directives(Queue, Kernel, InDescriptors, OutDescriptors) ->
 
   io:format("E1 = ~p~n", [cl:wait(E1)]),
   io:format("E2 = ~p~n", [cl:wait(E2)]),
-  io:format("E3 = ~p~n", [cl:wait(E3)]),
-
-  {ok, E3}.
-
-  
-
+  {ok, E3Res} = cl:wait(E3),
+  E3Res.
