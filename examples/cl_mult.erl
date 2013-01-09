1cdc246bc61fd1b4a157c33e1f07774dd85a0db3
Edward Tate <edward.tate@erlang-solutions.com>
2013-01-09 07:06:01 +0000

    Fleshed out cl_mult template.

--- examples/cl_mult.erl
+++ examples/cl_mult.erl
@@ -4,26 +4,45 @@
 
 -include("../include/cl.hrl").
 
+-define(GLOBAL_DIM, 1024).
+-define(LOCAL_DIM, 128).
+
+%%------------------------------------------------------------------------------
 test() ->
-  {cl, Platform, Devices, Context} = clu:setup(all),
+  Cl = clu:setup(all),
+
+  %%
+  %% Note: each kernel must have a source() and parameters() where parse_trans
+  %%       is concerned.
+  %%
   KernelSource = cl_mult_kernel:source(),
-  KernelDataDescriptors = cl_mult_kernel:data_descriptors(),
-  {ok, Kernel} = cl_util:build_kernel("mmult", Context, KernelSource, Devices),
-  BufferDescriptors = cl_util:create_buffers(Context, KernelDataDescriptors),
+  KernelI = cl_mult_kernel:input(),
+  KernelO = cl_mult_kernel:output(),
+  Kernel = cl_util:build_kernel("mmult", Cl#cl.context, KernelSource, Cl#cl.devices),
+
+  I = cl_util:create_buffers(Cl#cl.context, KernelI),
+  O = cl_util:create_buffers(Cl#cl.context, KernelO),
 
   CommandQueues = 
     lists:map(
       fun (Device) ->
-	  {ok, Queue} = cl:create_queue(Context, Device, []),
-	  Queue
-      end, Devices
+	  {ok, CommandQueue} = cl:create_queue(Cl#cl.context, Device, []),
+	  CommandQueue
+      end, Cl#cl.devices
      ),
   
   lists:foreach(
     fun (CommandQueue) ->
-	cl_util:enqueue_write_buffers(CommandQueue, BufferDescriptors)
+	execute_kernel_directives(CommandQueue, Kernel, I, O)
     end, CommandQueues
    ).
+
+execute_kernel_directives(Queue, Kernel, InDescriptors, OutDescriptors) ->  
+  MemI = cl_data:buffer_mem(InDescriptors),
+  MemO = cl_data:buffer_mem(OutDescriptors),
+  {ok, Event1} = cl_util:enqueue_write_buffers(Queue, InDescriptors),
+  cl_util:set_kernel_args(Kernel, MemI ++ MemO),
+  {ok, Event2} = cl:enqueue_nd_range_kernel(Queue, Kernel, [?GLOBAL_DIM], [?LOCAL_DIM], []),
+  {ok, Event3} = cl_util:enqueue_read_buffers(Queue, OutDescriptors).
   
 
-  
