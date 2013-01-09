d131ad79ff51047308c82430afc56dbce4cbe9cf
Edward Tate <edward.tate@erlang-solutions.com>
2013-01-09 16:11:14 +0000

    Added function which converts from a binary to a list of floats.

--- examples/cl_data.erl
+++ examples/cl_data.erl
@@ -1,6 +1,6 @@
 -module(cl_data).
 
--export([create_buffer/4, kernel_args/1]).
+-export([create_buffer/4, to_list/1, kernel_args/1]).
 
 %%------------------------------------------------------------------------------
 %% Generic type descriptors
@@ -10,7 +10,22 @@ create_buffer(Context, Data, TypeSize, Opt) ->
   {ok, Mem} = cl:create_buffer(Context, Opt, DataSize),
   {buffer_desc, Mem, Data, DataSize}.
 
+%%------------------------------------------------------------------------------
+%% @doc Convert a binary containing native float elements to a list of floats.
+%%------------------------------------------------------------------------------
+to_list(Binary) when is_binary(Binary) ->
+  to_list(Binary, []).
 
+to_list(<<>>, Acc) ->
+  lists:reverse(Acc);
+to_list(<<X:32/native-float, Rest/binary >>, Acc) ->
+  to_list(Rest, [X|Acc]).
+
+%%------------------------------------------------------------------------------
+%% @doc Convert a list of buffer_desc elements to kernel arguments. The 
+%%      following function returns a list which contains only atoms such as ints
+%%      and memory elements which can be used as arguments to an OpenCL kernel.
+%%------------------------------------------------------------------------------
 kernel_args(BufferDescriptors) ->
   kernel_args(BufferDescriptors, []).
 
