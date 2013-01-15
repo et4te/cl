ca0e10b2f8e131ba7d1f1aa556f8090e9ad2ad45
Edward Tate <edward.tate@erlang-solutions.com>
2013-01-15 13:12:05 +0000

    Modified the kernel to work with conitguous float data.

--- examples/cl_fft_kernel.erl
+++ examples/cl_fft_kernel.erl
@@ -2,9 +2,11 @@
 
 -export([source/0, input/0, output/0]).
 
--define(X_DIM, 16).
--define(Y_DIM, 16).
+-define(X_DIM, 16*2).
+-define(Y_DIM, 16*2).
 
+%%------------------------------------------------------------------------------
+%% Sample data
 %%------------------------------------------------------------------------------
 x() ->
   << <<X:32/native-float>> || X <- lists:seq(1, ?X_DIM) >>.
@@ -13,13 +15,18 @@ y() ->
   << <<Y:32/native-float>> || Y <- lists:seq(1, ?Y_DIM) >>.
 
 %%------------------------------------------------------------------------------
+%% Inputs / Outputs
+%%------------------------------------------------------------------------------
 input() ->
   [{x(), 4, [read_write]},
-   0].
+   4].
 
 output() ->
   [{y(), 4, [read_write]}].
 
+%%------------------------------------------------------------------------------
+%% Kernel source
+%%------------------------------------------------------------------------------
 source() ->
   "
 typedef float real_t;
@@ -48,7 +55,7 @@ real2_t twiddle(real2_t a, int k, real_t alpha)
 
 #define DFT2(a,b) { real2_t tmp = a - b; a += b; b = tmp; }
 
-__kernel void fftRadix2Kernel(__global const real2_t* x, int p, __global real2_t* y)
+__kernel void fftRadix2Kernel(__global const real_t* x, int p, __global real_t* y)
 {
   int t = get_global_size(0); // thread count
   int i = get_global_id(0);   // thread index
@@ -57,14 +64,17 @@ __kernel void fftRadix2Kernel(__global const real2_t* x, int p, __global real2_t
   real_t alpha = -FFT_PI*(real_t)k/(real_t)p;
   
   // read and twiddle input
-  x += i;
-  real2_t u0 = x[0];
-  real2_t u1 = twiddle(x[t],1,alpha);
+  x += i * 2;
+  real2_t u0 = (x[0],x[1]);
+  real2_t u1 = (x[t],x[t+1]);
+  real2_t u2 = twiddle(u1,1,alpha);
 
   // in-place DFT-2
-  DFT2(u0,u1);
+  DFT2(u0,u2);
 
-  y += j;
-  y[0] = u0;
-  y[p] = u1;
+  y += j * 2;
+  y[0] = u0.x;
+  y[1] = u0.y;
+  y[p] = u2.x;
+  y[p+1] = u2.y;
 }".
